d55c4d332105b09776b51379a4b727cfaff75e2e
4dmacau <tony@tonyklam.com>
2019-07-03 14:53:19 +0800

    Add SaveSystem

---
+++ Rhythm_adventure/Assets/Script/Manager/SaveSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using RhythmAssets;
+
+public static class SaveSystem
+{
+    public static void SaveData(Character_Warrior warrior)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/GameData";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData data = new GameData(warrior);
+        formatter.Serialize(stream, data);
+
+        stream.Close();
+    }
+    
+    public static GameData LoadData(Character_Warrior warrior)
+    {
+        string path = Application.persistentDataPath + "/GameData";
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+
+            return data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
