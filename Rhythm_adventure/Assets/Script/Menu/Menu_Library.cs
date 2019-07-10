1a5f87c6e3742fb81cd1b7dccd24aec0d03af297
tnab20022002@gmail.com <tnab20022002@gmail.com>
2019-07-11 01:28:37 +0800

    1. Upgrade engine version 2. Update Menu_Library 3. Add UI assets

---
+++ Rhythm_adventure/Assets/Script/Menu/Menu_Library.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Library : MonoBehaviour
+{
+    public string levelName = "";
+
+    [SerializeField] GameObject[] chapPenal;
+
+//    public Scene B = SceneManager.GetActiveScene();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Load_Level(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private int chapter = 0;
+    private int lastChapter = 1;
+    public void Switch_Chap(int chapter, bool isNextChap)
+    {
+        // 0 equal chapter 1
+        if(isNextChap)
+        {
+            if(chapter != lastChapter)
+            {
+                chapPenal[chapter].SetActive(false);
+                chapPenal[chapter + 1].SetActive(true);
+            }
+        }
+        else
+        {
+            if (chapter != 0)
+            {
+                chapPenal[chapter].SetActive(false);
+                chapPenal[chapter - 1].SetActive(true);
+            }
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
